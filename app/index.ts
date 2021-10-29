e9ea8e755bb7a4256508dc15e1e763b0080a8b95
AzureAD\四畝町浩康 <h_yosemachi@alta.co.jp>
2021-10-29 10:25:55 +0900

    環境追加

--- app/index.ts
+++ app/index.ts
@@ -1,7 +1,23 @@
-function greeter(person) {
-  return "Hello, " + person;
+class Student {
+  fullName: string;
+  constructor(
+    public firstName: string,
+    public middleInitial: string,
+    public lastName: string
+  ) {
+    this.fullName = firstName + " " + middleInitial + " " + lastName;
+  }
 }
 
-let user = "おはよう";
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+
+function greeter(person: Person) {
+  return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = new Student("Jane", "M.", "User");
 
 document.body.textContent = greeter(user);
